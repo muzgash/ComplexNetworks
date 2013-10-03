f483589d2feae495cc9be382aebe7744b324864a
muzgash <muzgash@gmali.com>
2013-10-03 10:34:36 -0500

    Awful way to create a WS network model. Functions added. TODO: Calculate the local and global cluster coefficients and the shortest path length. Insert every neighbor to every node, it doesn't matter the redundancy.

--- Watts-Strogatz/Watts-Strogatz.cpp
+++ Watts-Strogatz/Watts-Strogatz.cpp
@@ -6,24 +6,11 @@
 
 using namespace std;
 
-
-//Put this in error handling
-#define N 8
-//k must be even
-#define k 4
-#define p 0.5
-
 //Correct implementation of the module function
 int mod(int a, int b){return (a>=0) ? a%b : a%b+abs(b); }
 
-int main ( int argc, char* argv[] ) {
-
-    //Initialize the random number generators' seeds
-	srand48(time(NULL));
-	srand(time(NULL));
-
-    //Creating a regular ring
-    /*list<int> *graph;
+list<int>* regularRing(int N, int k) {
+    list<int> *graph;
 	graph = new list<int>[N];
 	for( int i = 0; i < N; i++ ) {
         int nodes[k];
@@ -33,18 +20,23 @@ int main ( int argc, char* argv[] ) {
 		}
 		list<int> tmp(nodes, nodes + sizeof(nodes) / sizeof(int));
 		graph[i]=tmp;
-	}*/
+	}
 
-	//The ring creation is not necessary since we know it's gonna change
-	//so to save a whole run through the graph we set the probabilities
-	//and create the WS graph from the beginning.
+	return graph;
+}
+
+list<int>* WSModel(int N,int k,double p) {
+
+	srand48(time(NULL));
+	srand(time(NULL));
+
 	list<int> *wsGraph;
 	wsGraph = new list<int>[N];
 	for( int i = 0; i < N; i++ ) {
         int nodes[k];
 		for( int j = -k/2; j <= k/2 ; j++ ) {
 		    if( j < 0 ) nodes[j+(k/2)] = mod(i+j,N);
-			else if( j > 0 ) {
+			if( j > 0 ) {
 			    if( drand48() < p ) { //If P -> rewire with a normal distribution
 			        nodes[j+(k/2)-1] = mod(random()%(k+1)+(i+1),7);
 			    }
@@ -54,16 +46,30 @@ int main ( int argc, char* argv[] ) {
 		list<int> tmp(nodes, nodes + sizeof(nodes) / sizeof(int));
 		wsGraph[i]=tmp;
 	}
-	
+    return wsGraph;
+}
 
-	//Displaying the ring
-	//#ifdef DEBUG
+void printGraph(list<int> *graph, int N) {
 	for( int i = 0; i < N;i++ ) {
-	    for( list<int>::iterator it = wsGraph[i].begin(); it != wsGraph[i].end(); it++){
+	    for( list<int>::iterator it = graph[i].begin(); it != graph[i].end(); it++){
 		    cout<<*it<<' ';
 		}
 		cout<<endl;
 	}
+}
+
+int main ( int argc, char* argv[] ) {
+    
+	int NN = 8, kk = 4;
+	double pp = 0.2;
+
+	list<int> *ws;
+
+	ws = WSModel(NN,kk,pp);
+	
+
+	//#ifdef DEBUG
+	printGraph(ws,NN);
 	//#endif
     return 0;
 }
